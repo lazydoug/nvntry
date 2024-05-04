33f92df4e493a4a015336431927c5f617b4b82ca
Osarodion Douglas Idumwonyi <98432474+lazydoug@users.noreply.github.com>
2024-05-04 05:39:39 -0400

    initialize passport middleware

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// app.use(passport.initialize())
+app.use(passport.initialize())
 
 app.use('/api/v1', routes)
 
